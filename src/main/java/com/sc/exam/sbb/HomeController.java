322ad5539eed37b494ef5f65931bc70a5043a5b0
atomic2000@naver.com <atomic2000@naver.com>
2024-04-18 08:26:16 +0900

    mbti, switch

--- src/main/java/com/sc/exam/sbb/HomeController.java
+++ src/main/java/com/sc/exam/sbb/HomeController.java
@@ -132,4 +132,20 @@ public class HomeController {
         .mapToObj(i -> "%d * %d = %d".formatted(finalDan, i, finalDan * i))
         .collect(Collectors.joining("<br>"));
   }
+
+  @GetMapping("/mbti/{name}")
+  @ResponseBody
+  public String showMbti(@PathVariable String name) {
+    System.out.println(name);
+
+    String rs = switch (name) {
+      case "홍길동" -> "INFP";
+      case "홍길순" -> "ENFP";
+      case "임꺽정" -> "ESFJ";
+      case "신짱구" -> "ENTP";
+      default -> "모름";
+    };
+
+    return rs;
+  }
 }
