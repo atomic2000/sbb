97e67634e482a4b409af5b52c48fbeb58d75aa7e
atomic2000@naver.com <atomic2000@naver.com>
2024-05-11 15:46:54 +0900

    Answer 엔티티 추가

--- src/main/java/com/sc/exam/sbb/Question.java
+++ src/main/java/com/sc/exam/sbb/Question.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 
 @Entity// 아래 Question 클래스는 엔티티 클래스이다.
 // 아래 클래스와 1:1로 매칭되는 테이블이 DB에 없다면, 자동으로 생성되어야 한다.
+@Table(name = "question", schema="sbb")
 public class Question {
   @Id // primary key
   @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
