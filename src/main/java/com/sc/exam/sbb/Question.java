153b41120178d2774a74f0aa214fb7247598b216
atomic2000@naver.com <atomic2000@naver.com>
2024-05-11 16:55:23 +0900

    Answer 엔티티 추가

--- src/main/java/com/sc/exam/sbb/Question.java
+++ src/main/java/com/sc/exam/sbb/Question.java
@@ -3,6 +3,7 @@ package com.sc.exam.sbb;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity// 아래 Question 클래스는 엔티티 클래스이다.
 // 아래 클래스와 1:1로 매칭되는 테이블이 DB에 없다면, 자동으로 생성되어야 한다.
@@ -20,4 +21,6 @@ public class Question {
 
   private LocalDateTime createDate; // Datetime
 
+  @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+  private List<Answer> answerList;
 }
