d9fc14adf2ef8fd79c66ef418b8df646a8241250
atomic2000@naver.com <atomic2000@naver.com>
2024-05-12 10:48:04 +0900

    findAll

--- src/test/java/com/sc/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/sc/exam/sbb/SbbApplicationTests.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -30,5 +34,20 @@ class SbbApplicationTests {
     q2.setContent("id는 자동으로 생성되나요?");
     q2.setCreateDate(LocalDateTime.now());
     this.questionRepository.save(q2); // 두번째 질문 저장
+
+    // id는 최소 0번보다 크다.
+    assertThat(q1.getId()).isGreaterThan(0);
+    // 두 번째 id는 첫 번째 id보다 크다.
+    assertThat(q2.getId()).isGreaterThan(q1.getId());
   }
+
+  @Test
+  void testJpa2() {
+    List<Question> all = questionRepository.findAll();
+    assertEquals(4, all.size());
+
+    Question q = all.get(0);
+    assertEquals("sbb가 무엇인가요?", q.getSubject());
+  }
+
 }
