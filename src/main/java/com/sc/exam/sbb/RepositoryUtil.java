2efc7aa19b3912c4bde446e2f8426b1c024b196a
atomic2000@naver.com <atomic2000@naver.com>
2024-05-15 19:11:23 +0900

    default 메서드를 도입하여 중복을 좀 더 제거

--- src/main/java/com/sc/exam/sbb/RepositoryUtil.java
+++ src/main/java/com/sc/exam/sbb/RepositoryUtil.java
@@ -14,4 +14,13 @@ public interface RepositoryUtil {
   @Modifying
   @Query(value = "SET FOREIGN_KEY_CHECKS = 1", nativeQuery = true)
   void enableForeignKeyChecks();
+
+  // default 메서드를 구현하면 인터페이스 내에 구상 메서드를 정의하는게 가능하다.
+  default void truncateTable() {
+    disableForeignKeyChecks();
+    truncate();
+    enableForeignKeyChecks();
+  }
+
+  void truncate();
 }
