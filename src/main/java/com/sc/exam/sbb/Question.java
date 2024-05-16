bf28136bfc019ae7609c4abc6f720882b1f96c51
atomic2000@naver.com <atomic2000@naver.com>
2024-05-16 11:07:28 +0900

    question으로부터_관련된_답변들_조회

--- src/main/java/com/sc/exam/sbb/Question.java
+++ src/main/java/com/sc/exam/sbb/Question.java
@@ -25,6 +25,6 @@ public class Question {
 
   private LocalDateTime createDate; // Datetime
 
-  @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+  @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
   private List<Answer> answerList;
 }
--- src/test/java/com/sc/exam/sbb/AnswerRepositoryTests.java
+++ src/test/java/com/sc/exam/sbb/AnswerRepositoryTests.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.LIST;
 
 @SpringBootTest
 class AnswerRepositoryTests {
@@ -73,10 +75,58 @@ class AnswerRepositoryTests {
   }
 
   @Test
+  // 답변을 통해서 질문을 조회
   void 관련된_question_조회() {
     Answer a = answerRepository.findById(1).get();
     Question q = a.getQuestion();
 
     assertThat(q.getId()).isEqualTo(1);
   }
+
+  @Test
+  void question으로부터_관련된_답변들_조회() {
+    // SELECT * FROM question WHERE id = 1;
+    Question q = questionRepository.findById(1).get();
+
+    // SELECT * FROM answer WHERE question_id = 1;
+    List<Answer> answerLIST = q.getAnswerList(); // DB 연결이 끊김.
+
+    assertThat(answerLIST.size()).isEqualTo(2);
+    assertThat(answerLIST.get(0).getContent()).isEqualTo("sbb는 질문답변 게시판입니다.");
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
