19b6bccf18a6e860f304872b1914bb810bf65758
atomic2000@naver.com <atomic2000@naver.com>
2024-05-18 19:23:15 +0900

    QuestionService 도입

--- src/main/java/com/sc/exam/sbb/question/QuestionController.java
+++ src/main/java/com/sc/exam/sbb/question/QuestionController.java
@@ -9,15 +9,22 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor // 생성자 주입
+// 컨트롤러는 Respository가 있는지 몰라야 한다.
+// 서비스는 브라우저라는 것이 이 세상에 존재하는지 몰라야 한다.
+// 리포지터리는 서비스가 이 세상에 있는지 몰라야 한다.
+// 서비스는 컨트롤러를 몰라야 한다.
+// DB는 리포지터리를 몰라야한다.
+// SPRING DATA JPA는 MySQL을 몰라야 한다.
+// SPRING DATA JPA(리포지터리) -> JPA -> 하이버네이트 -> JDBC -> MySQL Driver -> MySQL
 public class QuestionController {
 
   // @Autowired 필드 주입
-  private final QuestionRepository questionRepository;
+  private final QuestionService questionService;
 
   @GetMapping("/question/list")
   // 이 자리에 @ResponseBody가 없으면 resources/templates/question_list.html을 뷰로 삼는다.
   public String list(Model model) {
-    List<Question> questionList = questionRepository.findAll();
+    List<Question> questionList = questionService.getList();
 
     // 미리 실행된 question_list.html에서
     // questionList 라는 이름으로 questionList 변수를 사용할 수 있다.
