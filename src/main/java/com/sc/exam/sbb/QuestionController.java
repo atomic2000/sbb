c9121fb9af2f530b811eeba9181b1bf605508071
atomic2000@naver.com <atomic2000@naver.com>
2024-05-18 16:25:12 +0900

    question_list.html 뷰 파일 추가

--- src/main/java/com/sc/exam/sbb/QuestionController.java
+++ src/main/java/com/sc/exam/sbb/QuestionController.java
@@ -8,8 +8,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class QuestionController {
 
   @GetMapping("/question/list")
-  @ResponseBody
+  // 이 자리에 @ResponseBody가 없으면 resources/templates/question_list.html을 뷰로 삼는다.
   public String list() {
     return "question list";
+  }
+
+  @GetMapping("/question/list2")
+  // 이 자리에 @ResponseBody가 없으면 resources/templates/question_list.html을 뷰로 삼는다.
+  public String list2() {
+    return "question list2";
   }
 }
