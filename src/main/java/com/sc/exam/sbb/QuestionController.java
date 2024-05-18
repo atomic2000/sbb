b82621f5625f27ad70dd9c9d2d3e0c4a46defaf6
atomic2000@naver.com <atomic2000@naver.com>
2024-05-18 17:09:18 +0900

    질문 리스트를 표 형태로 보여주기

--- src/main/java/com/sc/exam/sbb/QuestionController.java
+++ src/main/java/com/sc/exam/sbb/QuestionController.java
@@ -1,21 +1,31 @@
 package com.sc.exam.sbb;
 
+import com.sc.exam.sbb.question.Question;
+import com.sc.exam.sbb.question.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor // 생성자 주입
 public class QuestionController {
+
+  // @Autowired 필드 주입
+  private final QuestionRepository questionRepository;
 
   @GetMapping("/question/list")
   // 이 자리에 @ResponseBody가 없으면 resources/templates/question_list.html을 뷰로 삼는다.
-  public String list() {
-    return "question list";
-  }
+  public String list(Model model) {
+    List<Question> questionList = questionRepository.findAll();
 
-  @GetMapping("/question/list2")
-  // 이 자리에 @ResponseBody가 없으면 resources/templates/question_list.html을 뷰로 삼는다.
-  public String list2() {
-    return "question list2";
+    // 미리 실행된 question_list.html에서
+    // questionList 라는 이름으로 questionList 변수를 사용할 수 있다.
+    model.addAttribute("questionList", questionList);
+    return "question_list";
   }
 }
