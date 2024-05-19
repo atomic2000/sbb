4f92d40bc26dd58ab8930fb01e4068a8702345fa
atomic2000@naver.com <atomic2000@naver.com>
2024-05-19 18:23:22 +0900

    폼 밸리데이션 적용하여 기존 로직 제거

--- src/main/java/com/sc/exam/sbb/question/QuestionController.java
+++ src/main/java/com/sc/exam/sbb/question/QuestionController.java
@@ -1,8 +1,10 @@
 package com.sc.exam.sbb.question;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,24 +48,13 @@ public class QuestionController {
   }
 
   @GetMapping("/create")
-  public String questionCreate() {
+  public String questionCreate(QuestionForm questionForm) {
     return "question_form";
   }
 
   @PostMapping("/create")
-  public String questionCreate(Model model, QuestionForm questionForm) {
-    boolean hasError = false;
-    if (questionForm.getSubject() == null || questionForm.getSubject().trim().length() == 0) {
-      model.addAttribute("subjectErrorMsg", "제목 좀 입력...");
-      hasError = true;
-    }
-    if (questionForm.getContent() == null || questionForm.getContent().trim().length() == 0) {
-      model.addAttribute("contentErrorMsg", "내용 좀 입력...");
-      hasError = true;
-    }
-
-    if(hasError) {
-      model.addAttribute("questionForm", questionForm);
+  public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+    if(bindingResult.hasErrors()) {
       return "question_form";
     }
 
