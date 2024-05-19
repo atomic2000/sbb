42f983fc2b9a466ea32549d6a9e5fd3d3aa68373
atomic2000@naver.com <atomic2000@naver.com>
2024-05-19 19:27:46 +0900

    작성일 기준으로 역순 정렬

--- src/main/java/com/sc/exam/sbb/question/QuestionService.java
+++ src/main/java/com/sc/exam/sbb/question/QuestionService.java
@@ -5,9 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,7 +19,11 @@ public class QuestionService {
   private final QuestionRepository questionRepository;
 
   public Page<Question> getList(int page) {
-    Pageable pageable = PageRequest.of(page, 10); // 한 페이지당 10개까지 보여주겠다.
+    List<Sort.Order> sorts = new ArrayList<>();
+    sorts.add(Sort.Order.desc("createDate"));
+    // sorts.add(Sort.Order.desc("id"));
+
+    Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts)); // 한 페이지당 10개까지 보여주겠다.
     return questionRepository.findAll(pageable);
   }
 
