35186a1eb4e343c7c0c1129dcd0b36f6d6d1b29d
atomic2000@naver.com <atomic2000@naver.com>
2024-05-23 08:45:04 +0900

    username 중복 체크

--- src/main/java/com/sc/exam/sbb/user/UserController.java
+++ src/main/java/com/sc/exam/sbb/user/UserController.java
@@ -2,6 +2,7 @@ package com.sc.exam.sbb.user;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,7 +30,19 @@ public class UserController {
       bindingResult.rejectValue("password2", "passwordInCorrect","2개의 패스워드가 일치하지 않습니다.");
       return "signup_form";
     }
-    userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1());
+
+    try {
+      userService.create(userCreateForm.getUsername(),userCreateForm.getEmail(),userCreateForm.getPassword1());
+    } catch (DataIntegrityViolationException e) {
+      e.printStackTrace();
+      bindingResult.reject("singnupFailed", "이미 등록된 사용자입니다.");
+      return "signup_form";
+    } catch (Exception e) {
+      e.printStackTrace();
+      bindingResult.reject("signupFailed", e.getMessage());
+      return "signup_form";
+    }
+
     return "redirect:/";
   }
 }
