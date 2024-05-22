7d770e2de99651381d729b84d75f8d92b4ed8be7
atomic2000@naver.com <atomic2000@naver.com>
2024-05-23 08:07:37 +0900

    PasswordEncoder객체의 생성을 스프링이 관리하도록

--- src/main/java/com/sc/exam/sbb/SecurityConfig.java
+++ src/main/java/com/sc/exam/sbb/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -23,5 +25,12 @@ public class SecurityConfig {
             .addHeaderWriter(new XFrameOptionsHeaderWriter(
                 XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)));
     return http.build();
+  }
+
+  // 스프링 시스템에 객체를 등록한다.
+  // @Configuration 라는 어노테이션을 가진 클래스에서만 사용이 가능
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
